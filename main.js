0e3d3d2379175d58a71e38373f95ef0d4be0e33f
Arthz933 <arthurassuncao017@gmail.com>
2026-09-21 11:14:17 -0300

    adicionando interatividade com js

---
+++ main.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll('.slide-img').forEach(img => observer.observe(img));
